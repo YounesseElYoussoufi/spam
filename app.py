9a63ce8592f9c7b2b4950921f245ac89ed4bd03f
YounesseElYoussoufi <116027860+YounesseElYoussoufi@users.noreply.github.com>
2023-10-21 17:13:40 +0000

    modifie app

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
 
-
+nltk.download('punkt')
+nltk.download('stopwords')
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
